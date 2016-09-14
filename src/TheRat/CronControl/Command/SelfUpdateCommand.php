bb90fe49df6a9b858f7a5a7ba7b2e37c1486c298
Vladimir Pak <TheRatW@gmail.com>
2016-09-14 10:08:09 +0300

    updat box json

--- src/TheRat/CronControl/Command/SelfUpdateCommand.php
+++ src/TheRat/CronControl/Command/SelfUpdateCommand.php
@@ -4,7 +4,6 @@ namespace TheRat\CronControl\Command;
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -15,77 +14,19 @@ class SelfUpdateCommand extends AbstractCommand
 {
     const MANIFEST_URI = 'https://raw.githubusercontent.com/TheRatG/cron-control/gh-pages/manifest.json';
 
-    /**
-     * The manifest file URI.
-     *
-     * @var string
-     */
-    private $manifestUri;
-
-    /**
-     * SelfUpdateCommand constructor.
-     * @param null|string $name
-     */
-    public function __construct($name)
-    {
-        parent::__construct($name);
-
-        $this->setManifestUri(self::MANIFEST_URI);
-    }
-
-    /**
-     * Sets the manifest URI.
-     *
-     * @param string $uri The URI.
-     */
-    public function setManifestUri($uri)
-    {
-        $this->manifestUri = $uri;
-    }
-
     protected function configure()
     {
         $this
             ->setName('self-update')
-            ->setDescription('Updates cron-control.phar to the latest version')
-            ->addOption(
-                'pre',
-                'p',
-                InputOption::VALUE_NONE,
-                'Allow pre-release updates.',
-                true
-            )
-            ->addOption(
-                'redo',
-                'r',
-                InputOption::VALUE_NONE,
-                'Redownload update if already using current version.'
-            )
-            ->addOption(
-                'upgrade',
-                'u',
-                InputOption::VALUE_NONE,
-                'Upgrade to next major release, if available.',
-                true
-            );
+            ->setDescription('Updates application to the latest version');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (is_null($this->manifestUri)) {
-            throw new \LogicException(
-                'No manifest URI has been configured.'
-            );
-        }
-
         $output->writeln('Looking for updates...');
 
         $manager = new Manager(Manifest::loadFile(self::MANIFEST_URI));
-        $res = $manager->update(
-            $this->getApplication()->getVersion(),
-            $input->getOption('upgrade'),
-            $input->getOption('pre')
-        );
+        $res = $manager->update($this->getApplication()->getVersion(), true, true);
         if ($res) {
             $output->writeln('<info>Update successful!</info>');
         } else {
